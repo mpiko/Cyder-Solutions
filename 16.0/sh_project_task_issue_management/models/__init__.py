58f6356b72e43e7bcfd035c4194556fa27f6df8d
Michael Piko <michael@piko.com.au>
2024-02-12 09:37:03 +1100

    removed 16

--- 16.0/sh_project_task_issue_management/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Softhealer Technologies.
-from . import sh_issue_type
-from . import sh_issue
-from . import project_project
-from . import project_task
-from . import sh_issue_stages
-from . import res_company
-from . import res_config_settings

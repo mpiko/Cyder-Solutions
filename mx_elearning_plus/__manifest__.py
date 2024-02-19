960c7964cbfb3e4af00fa88945259d92039e3220
Michael Piko <michael@piko.com.au>
2024-02-19 17:26:19 +1100

    added dummy module for upgrade

---
+++ mx_elearning_plus/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Mx_elearning_plus',
+    'version': '1.0.0',
+    'category': 'Business',
+    'author': 'Cyder Solutions',
+    'website': 'https://www.cyder.com.au/',
+    'sequence': -100,
+    'summary': 'Mx_elearning_plus',
+    'description': """
+    Mx_elearning_plus Program
+    """,
+    'depends': ['mail'],
+    'data': [
+        #'security/mx_elearning_plus_groups.xml',
+        #'security/ir.model.access.csv',
+        #'data/mx_elearning_plus_sequence.xml',
+        #'reports/report.xml',
+        #'reports/mx_elearning_plus_report.xml',
+        #'views/top_menu.xml',
+        #'views/mx_elearning_plus_view.xml',
+        #'views/mx_elearning_plus_category_view.xml',
+    ],
+    'demo': [],
+    #'images': ['static/description/banner.gif'],
+    'application': True,
+    'auto_install': False,
+    'license': 'OPL-1'
+}

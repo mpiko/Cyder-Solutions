7dfdf287639998f3a8c18e05403ecfe02583a0b9
Michael Piko <michael@piko.com.au>
2024-03-01 05:56:31 +1100

    removed

--- hide_qty-unitcost/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    hide = fields.Boolean(string='Hide Qty and Unit Cost', help="Hide quantity and unit price on sales reports")
-

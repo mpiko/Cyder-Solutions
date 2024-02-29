70cbb5612af9520b2eaa7c08f5d3922eb4ffcb0b
Michael Piko <michael@piko.com.au>
2024-02-29 12:34:49 +1100

    removed

--- mx_elearning_plus/models/mx_elearning_plus_category.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models
-class Mx_elearning_plusCategory(models.Model):
-    _name = "mx_elearning_plus.category"
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _description = "Mx_elearning_plus Category"
-
-    name = fields.Char('Category Name')
-    active = fields.Boolean(default=True)

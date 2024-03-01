65d8a187c8b7fe9b562d18bbe16a0e28be332b83
Michael Piko <michael@piko.com.au>
2024-03-01 19:07:23 +1100

    removed

--- cs_vehicle_log/models/hr_employee.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models
-
-class Cs_vehicle_logHrEmployee(models.Model):
-    _inherit = 'hr.employee'
-
-    preferred_vehicle = fields.Many2one('cs_vehicle_log.vehicles', "Preferred Vehicle")
